4fbc4f35d3f9ef005e99d5def03bf6b51e9d4129
Dave Irvine <dave@daveirvine.me>
2020-12-07 12:05:57 +0000

    Use UPSERT

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function run() {
                 ChangeBatch: {
                     Changes: [
                         {
-                            Action: "CREATE",
+                            Action: "UPSERT",
                             ResourceRecordSet: {
                                 Name: name,
                                 ResourceRecords: records,
